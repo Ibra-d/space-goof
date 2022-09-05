bbda83afdaf577d1bba21369bbd5eb1896b559b3
DRAME-ibrahima <drame.ibou.07@gmail.com>
2022-09-05 09:12:56 +0200

    form updated

--- pages/inscription.vue
+++ pages/inscription.vue
@@ -1,16 +1,11 @@
 <template>
-  <div class="inscriptionCodeContainer">
+  <div class="inscription__form__container">
     <h1>INSCR...</h1>
   </div>
 </template>
 
 <script>
-import {
-  onMounted,
-  ref,
-  useRouter,
-  useStore,
-} from '@nuxtjs/composition-api'
+import { onMounted, ref, useRouter, useStore } from '@nuxtjs/composition-api'
 export default {
   name: 'InscriptionPage',
   setup() {
